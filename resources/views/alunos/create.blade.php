c8449b2919afac12632e9e1dda418c979ddb76dd
Fabricio-Lubczyk <fabriciosamuel737@gmail.com>
2026-09-07 17:37:50 -0300

    ATV 14: Crie o formulário de cadastro de Aluno;

--- resources/views/alunos/create.blade.php
+++ resources/views/alunos/create.blade.php
@@ -7,8 +7,16 @@
 
     <form method="POST" action="{{ url('/alunos') }}">
         @csrf
+
         <label for="nome">Nome</label>
-        <input type="text" id="nome" name="nome">
+        <input type="text" id="nome" name="nome" value="{{ old('nome') }}" required>
+
+        <label for="email">E-mail</label>
+        <input type="email" id="email" name="email" value="{{ old('email') }}" required>
+
+        <label for="curso">Curso</label>
+        <input type="text" id="curso" name="curso" value="{{ old('curso') }}" required>
+
         <button type="submit">Cadastrar</button>
     </form>
 @endsection
