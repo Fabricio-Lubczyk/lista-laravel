e7b0e6627f776795840fb322951d8af4f6f4866d
Fabricio-Lubczyk <fabriciosamuel737@gmail.com>
2026-09-07 16:44:31 -0300

    ATV 1 cria rotas de texto

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre', function () {
+    return 'Página sobre o projeto';
+});
+
+Route::get('/alunos', function () {
+    return 'Lista de alunos';
+});
+
+Route::get('/contato', function () {
+    return 'Entre em contato conosco';
+});
